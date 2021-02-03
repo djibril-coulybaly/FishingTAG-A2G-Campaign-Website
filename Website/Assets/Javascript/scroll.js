7b3c1677f83b400908cb87cb264cd6d0841f936d
Djibril Coulybaly <46174096+djibril-coulybaly@users.noreply.github.com>
2021-02-04 00:37:14 +0900

    Create scroll.js
    This Javascript file contains the code to allow click to section of the website and to change the navigation colour when scrolling begins

---
+++ Website/Assets/Javascript/scroll.js
@@ -0,0 +1,41 @@
+window.onscroll = () => {
+    const nav = document.querySelector('#navbar');
+    if(this.scrollY <= 5) nav.className = ''; 
+    else nav.className = 'scroll';
+};
+
+function scrollToAboutCampaign(){
+    var element = document.getElementById('about-campaign');
+    var headerOffset = 100;
+    var elementPosition = element.offsetTop;
+    var offsetPosition = elementPosition - headerOffset;
+
+    window.scrollTo({
+        top: offsetPosition,
+        behavior: "smooth"
+    });
+}
+
+function scrollToHowItWorks(){
+    var element = document.getElementById('how-it-works');
+    var headerOffset = 100;
+    var elementPosition = element.offsetTop;
+    var offsetPosition = elementPosition - headerOffset;
+
+    window.scrollTo({
+        top: offsetPosition,
+        behavior: "smooth"
+    });
+}
+
+function scrollToDonateNow(){
+    var element = document.getElementById('donate-now');
+    var headerOffset = 100;
+    var elementPosition = element.offsetTop;
+    var offsetPosition = elementPosition - headerOffset;
+
+    window.scrollTo({
+        top: offsetPosition,
+        behavior: "smooth"
+    });
+}
