823678913bace8928fec5fce7a8e796bda7d40bc
Djibril Coulybaly <46174096+djibril-coulybaly@users.noreply.github.com>
2021-02-08 15:03:53 +0900

    Update to Website
    Changes made from local with Ji-hun and Ye-jin

--- Website/donationForm.php
+++ Website/donationForm.php
@@ -7,6 +7,8 @@
 
         <!-- Custom CSS Stylesheet -->
         <link rel="stylesheet" href="Assets\CSS\styles.css">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap" rel="stylesheet"> 
     </head>
     
@@ -18,7 +20,7 @@
                 <div class="wrapper nav_container">
                     <!-- Logo -->
                     <div class="logo">
-                        <a href="index.html"><img src="Assets/Images/navLogo.jpg" alt=""></a>
+                        <a href="index.html"><img src="Assets/Images/피싱태그 로고_150x100.png" alt=""></a>
                     </div>
 
                     <!-- Re-directs to section of webpage -->
@@ -39,44 +41,42 @@
                     </div>
                 </div>
             </nav>
-            
-            <?php
-				require_once "db.php";
-				if(isset($_POST['firstName']) && isset($_POST['lastName']) && isset($_POST['emailAddress']) && isset($_POST['mobileNumber']) && isset($_POST['addressLine1']) && isset($_POST['addressLine2']) && isset($_POST['city']) && isset($_POST['state']) && isset($_POST['zipPostcode']))
-				{
-					// Declaring and Initializing variables from the registration form
-					$fname = $_POST['firstName'];
-                    $lname = $_POST['lastName'];
-                    $email = $_POST['emailAddress'];
-                    $mob = $_POST['mobileNumber'];
-                    $add1 = $_POST['addressLine1'];
-					$add2 = $_POST['addressLine2'];
-					$c = $_POST['city'];
-                    $s = $_POST['state'];
-                    $zip = $_POST['zipPostcode'];
-					
-					// Error checking the form to make sure it complies with the specifications given
-					if($fname == '' || $lname == '' || $email == '' || $mob == '' || $add1 == '' || $add2 == '' || $c == '' || $s == '' || $zip == '')
-					{
-						echo "<p class='error'>All fields must be filled in order to continue</p>";
-					}
-					else
-					{
-						$sql = "INSERT INTO shippingList (FirstName, LastName, EmailAddress, MobileNumber, AddressLine1, AddressLine2, City, State, Zip) VALUES ('$fname', '$lname', '$email', '$mob', '$add1', '$add2', '$c', '$s', '$zip')";
-						// Inserting the user's details into the database and displaying a confirmation message 
-						// to the user with a link to return to the login page
-						mysqli_query($db, $sql);
-						echo '<p>Congratulations! Your account was created successfully. Click <a href="login.html">here</a> to log into My Novella Account</p>';
-					}
-				}
-			?>
-
 
             <!-- Donation Form -->
             <div class="donation" id="donate-now">
                 <h1>Thank you for your donation ^_^</h1>
                 <h2>All that's left to do is to fill in your shipping details to recieve your Smart Measure Tag!</h2>
                 <div class="donation-form">    
+                    <?php
+                        require_once "db.php";
+                        if(isset($_POST['firstName']) && isset($_POST['lastName']) && isset($_POST['emailAddress']) && isset($_POST['mobileNumber']) && isset($_POST['addressLine1']) && isset($_POST['addressLine2']) && isset($_POST['city']) && isset($_POST['state']) && isset($_POST['zipPostcode']))
+                        {
+                            // Declaring and Initializing variables from the registration form
+                            $fname = $_POST['firstName'];
+                            $lname = $_POST['lastName'];
+                            $email = $_POST['emailAddress'];
+                            $mob = $_POST['mobileNumber'];
+                            $add1 = $_POST['addressLine1'];
+                            $add2 = $_POST['addressLine2'];
+                            $c = $_POST['city'];
+                            $s = $_POST['state'];
+                            $zip = $_POST['zipPostcode'];
+                            
+                            // Error checking the form to make sure it complies with the specifications given
+                            if($fname == '' || $lname == '' || $email == '' || $mob == '' || $add1 == '' || $add2 == '' || $c == '' || $s == '' || $zip == '')
+                            {
+                                echo "<p class='error'>All fields must be filled in order to continue</p>";
+                            }
+                            else
+                            {
+                                $sql = "INSERT INTO shippingList (FirstName, LastName, EmailAddress, MobileNumber, AddressLine1, AddressLine2, City, State, Zip) VALUES ('$fname', '$lname', '$email', '$mob', '$add1', '$add2', '$c', '$s', '$zip')";
+                                // Inserting the user's details into the database and displaying a confirmation message 
+                                // to the user with a link to return to the login page
+                                mysqli_query($db, $sql);
+                                header('Location:thankYou.php');
+                            }
+                        }
+                    ?>
                     <form method="POST">
                         <div class="user-box">
                             <input type="text" name="firstName" id="firstName" required="">
@@ -132,7 +132,7 @@
             <footer class="parallax">
                 <!-- FishingTAG Logo -->
                 <div class="footerLogo">
-                    <img src="Assets/Images/displayLogo.jpg" alt="">
+                    <img src="Assets/Images/피싱태그 로고_150x100.png" alt="">
                 </div>
                 
                 <!-- Copyright Information -->
