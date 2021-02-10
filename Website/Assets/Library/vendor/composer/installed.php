3081a5c21964ebb83150d545aca0c5c936a6b0b4
Djibril Coulybaly <46174096+djibril-coulybaly@users.noreply.github.com>
2021-02-10 11:57:06 +0900

    Composer Library
    Added Composer Library for PHPMailer and Paypal API

---
+++ Website/Assets/Library/vendor/composer/installed.php
@@ -0,0 +1,42 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '44aa776490ee3cbf98ade590aa849f934e31156e',
+    'name' => 'me/shopping-cart-app',
+  ),
+  'versions' => 
+  array (
+    'me/shopping-cart-app' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '44aa776490ee3cbf98ade590aa849f934e31156e',
+    ),
+    'paypal/merchant-sdk-php' => 
+    array (
+      'pretty_version' => 'v3.12.0',
+      'version' => '3.12.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f21fe42ad787f98ea5d7186b19e8bec97f1852ed',
+    ),
+    'paypal/sdk-core-php' => 
+    array (
+      'pretty_version' => '3.4.0',
+      'version' => '3.4.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd2174f69f9811a8f2e79af7498fdf89586457d76',
+    ),
+  ),
+);
