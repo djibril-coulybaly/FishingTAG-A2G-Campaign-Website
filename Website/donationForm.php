5360892cb25c733b613a4de26a072a3cbca41bc3
Djibril Coulybaly <46174096+djibril-coulybaly@users.noreply.github.com>
2021-02-10 12:00:18 +0900

    PDT added to obtain Paypal Transaction Info
    Payment Data Transaction script in PHP added with the code to enter info into database

--- Website/donationForm.php
+++ Website/donationForm.php
@@ -1,3 +1,90 @@
+<?php
+    //look if the parameter 'tx' is set in the GET request and that it does not have a null or empty value
+    if(isset($_GET['tx']) && ($_GET['tx'])!=null && ($_GET['tx'])!= "") 
+    {
+        $pp_hostname = "www.sandbox.paypal.com"; // Change to www.sandbox.paypal.com to test against sandbox
+
+        // read the post from PayPal system and add 'cmd'
+        $req = 'cmd=_notify-synch';
+        $tx_token = $_GET['tx'];
+        $auth_token = "qZ3l08UybzfDIdyYvz4Oo-b1qtKPfpgJ1gWXcEx1SDFSkLBXoKF4vGL2rYu";
+        $req .= "&tx=$tx_token&at=$auth_token";
+
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "https://$pp_hostname/cgi-bin/webscr");
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $req);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1);
+        //set cacert.pem verisign certificate path in curl using 'CURLOPT_CAINFO' field here,
+        //if your server does not bundled with default verisign certificates.
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array("Host: $pp_hostname"));
+        $res = curl_exec($ch);
+        curl_close($ch);
+
+        if(!$res)
+        {
+            //HTTP ERROR
+            exit();
+        }
+        else
+        {
+            // parse the data
+            $lines = explode("\n", trim($res));
+            $keyarray = array();
+            if (strcmp ($lines[0], "SUCCESS") == 0) 
+            {
+                for ($i = 1; $i < count($lines); $i++) 
+                {
+                    $temp = explode("=", $lines[$i],2);
+                    $keyarray[urldecode($temp[0])] = urldecode($temp[1]);
+                }
+                
+                // check the payment_status is Completed
+                $paymentStatus = $keyarray["payment_status"];
+                if($paymentStatus!="Completed") 
+                {
+                    exit();
+                }
+
+                // check that txn_id has not been previously processed
+                require_once "db.php";
+                $sql = mysqli_query($db,"SELECT ShippingID FROM shippingList WHERE TransactionID='$tx_token' LIMIT 1");
+                $numRows = mysqli_num_rows($sql);
+                if ($numRows > 0) 
+                {
+                    exit();
+                }
+
+                // check that receiver_email is your Primary PayPal email
+                $myEmail = $keyarray['receiver_email'];
+                if ($myEmail != "djibril.coulybaly@gmail.com") 
+                {
+                    exit();
+                }
+
+                // check that payment_amount/payment_currency are correct
+                $amount = $keyarray['payment_gross'];
+                $currency = $keyarray["mc_currency"];
+                
+
+                // Proceed with filling out form details
+            }
+            else if (strcmp ($lines[0], "FAIL") == 0) 
+            {
+                // log for manual investigation
+            }
+        }
+    }
+    else 
+    {
+        exit();
+    }
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -8,7 +95,7 @@
         <!-- Custom CSS Stylesheet -->
         <link rel="stylesheet" href="Assets\CSS\styles.css">
         <link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap" rel="stylesheet"> 
     </head>
     
@@ -20,18 +107,18 @@
                 <div class="wrapper nav_container">
                     <!-- Logo -->
                     <div class="logo">
-                        <a href="index.html"><img src="Assets/Images/피싱태그 로고_150x100.png" alt=""></a>
+                        <a href="index.html"><img src="Assets/Images/피싱태그 로고만.png" alt=""></a>
                     </div>
 
                     <!-- Re-directs to section of webpage -->
                     <ul class="pageLink displayPageLink">
                         <!-- About the campaign -->
-                        <li><button onclick="scrollToAboutCampaign();" class="buttonHoverMargin"><p>About the campaign</p></button></li>
+                        <li><button onclick="AboutCampaign();" class="buttonHoverMargin"><p>About the campaign</p></button></li>
                         <!-- How it works -->
-                        <li><button onclick="scrollToHowItWorks();" class="buttonHoverMargin"><p>How it works</p></button></li>
+                        <li><button onclick="HowItWorks();" class="buttonHoverMargin"><p>How it works</p></button></li>
                         <!-- Donate Now Button -->
                         <li>
-                            <button id="buttonNav" onclick="scrollToDonateNow();"><p>Donate</p></button>
+                            <button id="buttonNav" onclick="DonateNow();"><p>Donate</p></button>
                         </li>
                     </ul>
 
@@ -69,7 +156,7 @@
                             }
                             else
                             {
-                                $sql = "INSERT INTO shippingList (FirstName, LastName, EmailAddress, MobileNumber, AddressLine1, AddressLine2, City, State, Zip) VALUES ('$fname', '$lname', '$email', '$mob', '$add1', '$add2', '$c', '$s', '$zip')";
+                                $sql = "INSERT INTO shippingList (FirstName, LastName, EmailAddress, MobileNumber, AddressLine1, AddressLine2, City, State, Zip, TransactionID) VALUES ('$fname', '$lname', '$email', '$mob', '$add1', '$add2', '$c', '$s', '$zip', '$tx_token')";
                                 // Inserting the user's details into the database and displaying a confirmation message 
                                 // to the user with a link to return to the login page
                                 mysqli_query($db, $sql);
@@ -132,7 +219,7 @@
             <footer class="parallax">
                 <!-- FishingTAG Logo -->
                 <div class="footerLogo">
-                    <img src="Assets/Images/피싱태그 로고_150x100.png" alt="">
+                    <a href="index.php"><img src="Assets/Images/피싱태그 로고_150x100.png" alt=""></a>
                 </div>
                 
                 <!-- Copyright Information -->
